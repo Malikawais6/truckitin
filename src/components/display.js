55f9cbfd0317bd7188ae8d16a9d5a8c0f16ba581
malikawais6 <malikxawais@gmail.com>
2021-09-01 12:25:11 +0500

    Calculator basic functionalities added

---
+++ src/components/display.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Display = (props) => {
+  return <div className="display">{props.result}</div>;
+};
+
+export default Display;
---
+++ src/container/calculator.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import * as math from "mathjs";
+
+import Button from "../components/button";
+import Display from "../components/display";
+
+import "./style.css";
+
+const Calculator = () => {
+  const [state, setState] = useState({
+    result: "",
+    options: [
+      ["(", "CE", ")", "C"],
+      ["1", "2", "3", "+"],
+      ["4", "5", "6", "-"],
+      ["7", "8", "9", "*"],
+      [".", "0", "=", "/"],
+    ],
+  });
+
+  const handleButtonActions = (val) => {
+    if (val === "=") {
+      setState((st) => ({
+        ...st,
+        result: math.evaluate(state.result),
+      }));
+    } else if (val === "C") {
+      setState((st) => ({
+        ...st,
+        result: "",
+      }));
+    } else if (val === "CE") {
+      setState((st) => ({
+        ...st,
+        result: state.result.slice(0, -1),
+      }));
+    } else {
+      setState((st) => ({
+        ...st,
+        result: state.result + val,
+      }));
+    }
+  };
+
+  return (
+    <div className="app">
+      <Display result={state.result} />
+      <div className="row-wrapper">
+        {state.options.map((row) => {
+          return (
+            <div className="row">
+              {row.map((digit) => {
+                return (
+                  <Button handleClick={handleButtonActions}>{digit}</Button>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default Calculator;
